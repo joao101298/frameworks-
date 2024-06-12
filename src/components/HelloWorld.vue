bf45455f9c3caafe065efb15cd2734a83ac1c7d6
joao101298 <joaovitornogueira555@gmail.com>
2024-06-12 20:44:01 -0300

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+ <h1>Olá, Mundo!</h1>
+ <p>Bem-vindo ao seu primeiro projeto Vue.js.</p>
+ </div>
+</template>
+
+<script>
+ export default {
+ name: 'HelloWorld',
+ }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .container {
+ text-align: center;
+ margin-top: 50px;
+ }
+</style>
